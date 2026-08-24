9f28d0f589745f43089fcfbcf9bb5857cd3cef21
Deepak <dk6625901@gmail.com>
2026-08-24 18:52:16 +0530

    Added opentelemetry and jaeger for traces and UI

---
+++ instrumentation.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-grpc");
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+
+const traceExporter = new OTLPTraceExporter({
+  url: "http://localhost:4317",
+});
+
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
